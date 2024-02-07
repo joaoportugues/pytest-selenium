333d3c6ff6b9a186aaf31a275f212aeb0ffd42f3
Joe <joaomatiasportugues@gmail.com>
2024-02-07 21:58:07 +0100

    stream screenshots to report

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pytest-selenium
+
+```
+pip install -r requirements.py
+```
+
+```
+pytest --html=report.html
+```
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import pytest_html
 import base64
 from selenium import webdriver
 
@@ -10,3 +11,19 @@ def driver():
     yield driver
     driver.quit()
 
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    extras = getattr(report, "extras", [])
+
+    if report.when == "call":
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # Add screenshot on failure
+            driver = item.funcargs.get('driver')
+            if driver:
+                screenshot = driver.get_screenshot_as_base64()
+                extras.append(pytest_html.extras.image(screenshot, "Screenshot"))
+    
+    report.extras = extras
